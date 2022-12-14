cce833d437dad326f94ae6a4929dffe938564230
RoxaneC <61699049+RoxaneC@users.noreply.github.com>
2022-12-14 12:34:55 +0100

    Update projet-global.pl

--- projet-global.pl
+++ projet-global.pl
@@ -17,7 +17,7 @@ nnf(not(X), not(X)) :- !.
 nnf(and(C1,C2), and(NC1,NC2)) :- nnf(C1,NC1), nnf(C2,NC2), !.
 nnf(or(C1,C2), or(NC1,NC2)) :- nnf(C1,NC1), nnf(C2,NC2), !.
 nnf(some(R,C), some(R,NC)) :- nnf(C,NC), !.
-nnf(all(R,C), all(R,NC)) :- nnf(C,NC), !.
+nnf(all(R,C), all(R,NC)tri_Abox) :- nnf(C,NC), !.
 nnf(X,X).
 
 
@@ -224,14 +224,36 @@ troisieme_etape(Abi,Abr) :-	tri_Abox(Abi,Lie,Lpt,Li,Lu,Ls),
 							resolution(Lie,Lpt,Li,Lu,Ls,Abr), nl,
 							write('Youpiiiiii, on a demontre la proposition initiale !!!').
 
+
+% Tri de la A_Box étendue
+tri_Abox([], [], [], [], [], []).
+tri_Abox([ (I,some(R,C)) | Abi], [ (I,some(R,C)) | Lie], Lpt, Li, Lu, Ls) :-	tri_Abox(Abi,Lie,Lpt,Li,Lu,Ls), !.
+tri_Abox([ (I,all(R,C)) | Abi], Lie, [ (I,all(R,C)) | Lpt], Li, Lu, Ls) :-		tri_Abox(Abi,Lie,Lpt,Li,Lu,Ls), !.
+tri_Abox([ (I,and(C1,C2)) | Abi], Lie, Lpt, [ (I,and(C1,C2)) | Li], Lu, Ls) :-	tri_Abox(Abi,Lie,Lpt,Li,Lu,Ls), !.
+tri_Abox([ (I,or(C1,C2)) | Abi], Lie, Lpt, Li, [ (I,or(C1,C2)) | Lu], Ls) :-	tri_Abox(Abi,Lie,Lpt,Li,Lu,Ls), !.
+tri_Abox([ (I,C) | Abi], Lie, Lpt, Li, Lu, [ (I,C) | Ls]) :-					tri_Abox(Abi,Lie,Lpt,Li,Lu,Ls), !.
+tri_Abox([ (I,not(C)) | Abi], Lie, Lpt, Li, Lu, [ (I,not(C)) | Ls]) :-			tri_Abox(Abi,Lie,Lpt,Li,Lu,Ls), !.
+
+
+
+complete_some([ (I1,some(R,C)) | Lie], Lpt, Li, Lu, Ls, Abr) :-	genere(I2),
+			evolue((I2,C), Lie, Lpt, Li, Lu, Ls, Lie1, Lpt1, Li1, Lu1, Ls1),
+			
+			resolution(Lie1, Lpt1, Li1, Lu1, Ls1, [ (I1, I2, R) | Abr]), !.
+
+
+
+evolue([], Lie, Lpt, Li, Lu, Ls, Lie, Lpt, Li, Lu, Ls).
+evolue((I,some(R,C)), Lie, Lpt, Li, Lu, Ls, Lie1, Lpt1, Li1, Lu1, Ls1) :- 	concat([ (I,some(R,C)) ], Lie, Lie1), !.
+evolue((I,all(R,C)), Lie, Lpt, Li, Lu, Ls, Lie1, Lpt1, Li1, Lu1, Ls1) :- 	concat([ (I,all(R,C)) ], Lpt, Lpt1), !.
+evolue((I,and(C1,C2)), Lie, Lpt, Li, Lu, Ls, Lie1, Lpt1, Li1, Lu1, Ls1) :- 	concat([ (I,and(C1,C2)) ], Li1, Li1), !.
+evolue((I,or(C1,C2)), Lie, Lpt, Li, Lu, Ls, Lie1, Lpt1, Li1, Lu1, Ls1) :- 	concat([ (I,or(C1,C2)) ], Lu, Lu1), !.
+evolue((I,C), Lie, Lpt, Li, Lu, Ls, Lie1, Lpt1, Li1, Lu1, Ls1) :- 			concat([ (I,C) ], Ls, Ls1), !.
+evolue((I,not(C)), Lie, Lpt, Li, Lu, Ls, Lie1, Lpt1, Li1, Lu1, Ls1) :- 		concat([ (I,not(C)) ], Ls, Ls1), !.
+
 %% A FAIRE
 
-tri_Abox(Abi,Lie,Lpt,Li,Lu,Ls) :-	setof()
-									, !.
-
-resolution(Lie,Lpt,Li,Lu,Ls,Abr) :- 	.
-
-complete_some(Lie,Lpt,Li,Lu,Ls,Abr) :- 	.
+resolution(Lie, Lpt, Li, Lu, Ls, Abr) :- 	.
 
 transformation_and(Lie,Lpt,Li,Lu,Ls,Abr) :- 	.
 
@@ -239,9 +261,6 @@ deduction_all(Lie,Lpt,Li,Lu,Ls,Abr) :- 	.
 
 transformation_or(Lie,Lpt,Li,Lu,Ls,Abr) :- 	.
 
-%% &
-
-evolue(A, Lie, Lpt, Li, Lu, Ls, Lie1, Lpt1, Li1, Lu1, Ls1) :- 	.
 
 affiche_evolution_Abox(Ls1, Lie1, Lpt1, Li1, Lu1, Abr1, Ls2, Lie2, Lpt2, Li2, Lu2, Abr2) :- .
 
